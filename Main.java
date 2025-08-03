bcf50766e4e5d262ff58132196f7ce07ea1bab07
Sushil Pal <sushilkumarpal8744@gmail.com>
2025-08-03 21:10:42 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Main {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
-    }
-}
